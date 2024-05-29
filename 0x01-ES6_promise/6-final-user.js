32c70f2355fcae7ec7a8599d2484f0917cc46715
Muna-Yusuf <mun.y.a.jim@gmail.com>
2024-05-29 20:13:56 +0000

    task 7.8

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,13 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((out) => ({
+        status: out.status,
+        value: out.status === 'fulfilled' ? out.value : String(out.reason),
+      }))
+    ));
+}
