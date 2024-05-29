b1e46a2f6660a4a763e5b7769dd2e6d8539b2173
Muna-Yusuf <mun.y.a.jim@gmail.com>
2024-05-29 18:53:17 +0000

    fixing

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,5 @@
-export default function getResponseFromAPI(){
-  return new Promise((resolve, reject) => {
-     resolve('API response');
+export default function getResponseFromAPI() {
+  return new Promise((resolve) => {
+    resolve('API response');
   });
 }
