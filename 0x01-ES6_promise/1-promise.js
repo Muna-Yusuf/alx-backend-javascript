5c7ee76a5a510c3fc7066c0ed5d18c8b5928288f
Muna-Yusuf <mun.y.a.jim@gmail.com>
2024-05-29 18:40:15 +0000

    fixing

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,5 +1,5 @@
 export default function getFullResponseFromAPI(success) {
-  return new promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
       if (success) {
           resolve({status: 200, body: 'success'});
       } else {
