a4fd0c6f1afc524c14a59ec84056ff8229a8f7a6
Muna-Yusuf <mun.y.a.jim@gmail.com>
2024-06-04 16:50:34 +0000

    fix

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,9 +1,9 @@
-export default function updateUniqueItems(item) {
-  if (Object.getPrototypeOf(item) !== Map.prototype) throw new TypeError('Cannot process');
-  item.forEach((quantity, name) => {
+export default function updateUniqueItems(map) {
+  if (Object.getPrototypeOf(map) !== Map.prototype) throw new TypeError('Cannot process');
+  map.forEach((quantity, name) => {
     if (quantity === 1) {
-      item.set(name, 100);
+      map.set(name, 100);
     }
   });
-  return item;
+  return map;
 }
