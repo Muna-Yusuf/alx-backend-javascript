165f507bdb9abc020d6bcd18b580269634d8accf
Muna-Yusuf <mun.y.a.jim@gmail.com>
2024-06-05 20:32:30 +0000

    removed

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,15 @@
+// main.ts
+/// <reference path="./crud.d.ts" />
+import { RowID, RowElement } from './interface';
+import * as CRUD from './crud';
+
+const row: RowElement = { firstName: "Guillaume", lastName: "Salva" };
+
+const newRowID: RowID = CRUD.insertRow(row);
+console.log(`CRUD.insertRow(obj)\n// Insert row ${JSON.stringify(row)}`);
+
+const updatedRow: RowElement = { ...row, age: 23 };
+CRUD.updateRow(newRowID, updatedRow);
+console.log(`CRUD.updateRow(${newRowID}, ${JSON.stringify(updatedRow)})`);
+CRUD.deleteRow(newRowID);
+console.log(`CRUD.deleteRow(${newRowID})`);
