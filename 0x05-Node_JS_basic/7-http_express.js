04d5bf7b59d0af6f69eecce316dba2a7b10c23fa
Muna-Yusuf <mun.y.a.jim@gmail.com>
2024-07-24 18:50:37 +0000

    fixing 7

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -4,35 +4,34 @@ const fs = require('fs');
 function countStudents(path) {
   return new Promise((resolve, reject) => {
     fs.readFile(path, { encoding: 'utf-8' }, (err, data) => {
-      if (err) {
-        return reject(new Error('Cannot load the database'));
-      }
-
+      if (err) return reject(Error('Cannot load the database'));
       const lines = data.split('\n').slice(1, -1);
-      const header = data.split('\n')[0].split(',');
-
-      const idxFn = header.indexOf('firstname');
-      const idxFd = header.indexOf('field');
-
+      const header = data.split('\n').slice(0, 1)[0].split(',');
+      const idxFn = header.findIndex((ele) => ele === 'firstname');
+      const idxFd = header.findIndex((ele) => ele === 'field');
       const fields = {};
       const students = {};
       const all = {};
 
       lines.forEach((line) => {
         const list = line.split(',');
-        const field = list[idxFd];
-        const firstname = list[idxFn];
-
-        fields[field] = (fields[field] || 0) + 1;
-        students[field] = students[field] ? `${students[field]}, ${firstname}` : firstname;
+        if (!fields[list[idxFd]]) fields[list[idxFd]] = 0;
+        fields[list[idxFd]] += 1;
+        if (!students[list[idxFd]]) students[list[idxFd]] = '';
+        students[list[idxFd]] += students[list[idxFd]]
+          ? `, ${list[idxFn]}`
+          : list[idxFn];
       });
 
       all.numberStudents = `Number of students: ${lines.length}\n`;
-      all.listStudents = Object.entries(fields).map(
-        ([field, count]) => `Number of students in ${field}: ${count}. List: ${students[field]}`
-      );
-
-      resolve(all);
+      all.listStudents = [];
+      for (const key in fields) {
+        if (Object.hasOwnProperty.call(fields, key)) {
+          const element = fields[key];
+          all.listStudents.push(`Number of students in ${key}: ${element}. List: ${students[key]}`);
+        }
+      }
+      return resolve(all);
     });
   });
 }
@@ -43,7 +42,6 @@ const port = 1245;
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
-
 app.get('/students', (req, res) => {
   res.write('This is the list of our students\n');
   countStudents(process.argv[2])
@@ -55,9 +53,6 @@ app.get('/students', (req, res) => {
       res.end(err.message);
     });
 });
-
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
-});
+app.listen(port);
 
 module.exports = app;
